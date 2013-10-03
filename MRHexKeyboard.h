0ef0020f04e08dff64211c1bb3750440f3f909de
Mikk Rätsep <mikk@cannedapps.com>
2013-10-03 13:59:35 +0300

    Added the code and delete-button image

---
+++ MRHexKeyboard.h
@@ -0,0 +1,12 @@
+//
+//  MRHexKeyboard.h
+//
+//  Created by Mikk Rätsep on 02/10/13.
+//  Copyright (c) 2013 Mikk Rätsep. All rights reserved.
+//
+
+@interface MRHexKeyboard : UIView
+
+- (MRHexKeyboard *)initWithTextField:(UITextField *)textField;
+
+@end
